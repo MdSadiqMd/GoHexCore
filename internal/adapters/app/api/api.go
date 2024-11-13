3682830a3ea207456444f5d81fd8158032b71f62
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-11-13 22:14:12 +0530

    🎩 Added database to api adapters

--- internal/adapters/app/api/api.go
+++ internal/adapters/app/api/api.go
@@ -3,15 +3,21 @@ package api
 import "github.com/MdSadiqMd/GoHexCore/internal/ports"
 
 type Adapter struct {
+	db    ports.DbPort
 	arith ports.APIPort
 }
 
-func NewAdapter(arith ports.APIPort) *Adapter {
-	return &Adapter{arith: arith}
+func NewAdapter(db ports.DbPort, arith ports.APIPort) *Adapter {
+	return &Adapter{db: db, arith: arith}
 }
 
 func (apia Adapter) GetAddition(a, b int32) (int32, error) {
 	answer, err := apia.arith.GetAddition(a, b)
+	if err != nil {
+		return 0, err
+	}
+
+	err = apia.db.AddToHistory(answer, "addition")
 	if err != nil {
 		return 0, err
 	}
@@ -23,6 +29,11 @@ func (aria Adapter) GetSubtraction(a, b int32) (int32, error) {
 	if err != nil {
 		return 0, err
 	}
+
+	err = aria.db.AddToHistory(answer, "subtraction")
+	if err != nil {
+		return 0, err
+	}
 	return answer, nil
 }
 
@@ -31,11 +42,21 @@ func (aria Adapter) GetMultiplication(a, b int32) (int32, error) {
 	if err != nil {
 		return 0, err
 	}
+
+	err = aria.db.AddToHistory(answer, "multiplication")
+	if err != nil {
+		return 0, err
+	}
 	return answer, nil
 }
 
 func (aria Adapter) GetDivision(a, b int32) (int32, error) {
 	answer, err := aria.arith.GetDivision(a, b)
+	if err != nil {
+		return 0, err
+	}
+
+	err = aria.db.AddToHistory(answer, "division")
 	if err != nil {
 		return 0, err
 	}
